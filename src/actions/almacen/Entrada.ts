32516d0b1dd745667d762d99b125c3cbf74dba91
Alejandro <barretoalejandro21@gmail.com>
2025-12-14 17:03:25 -0400

    Segundo avance modulo entrada

--- src/actions/almacen/Entrada.ts
+++ src/actions/almacen/Entrada.ts
@@ -1,6 +1,6 @@
 import { defineAction } from "astro:actions";
 import { z } from "astro:schema";
-import { getEntries } from "../../models/almacen/entrada";
+import { getEntries, addSupplier, asingSupplierType, addSupplierContact, addSupplierDirection } from "../../models/almacen/entrada";
 
 export const entrada = ({
     getEntries: defineAction({
@@ -9,4 +9,92 @@ export const entrada = ({
             return entries;
         }
     }),
+    addSupplier: defineAction({
+        input: z.object({
+            nameProveedor: z.string(),
+            rifProveedor: z.number(),
+            estadoProveedor: z.boolean(),
+        }),
+        async handler({ nameProveedor, rifProveedor, estadoProveedor }) {
+            const { data, error } = await addSupplier(nameProveedor, 'RIF', rifProveedor, estadoProveedor);
+
+            if (error) {
+                return {
+                    status: false,
+                    message: 'Error al agregar el proveedor: ' + error,
+                };
+            }
+
+            return {
+                status: true,
+                message: 'Proveedor agregado exitosamente',
+                idpersona: data
+            };
+        }
+    }),
+    asingSupplierType: defineAction({
+        input: z.object({
+            idpersona: z.number(),
+        }),
+        async handler({ idpersona }) {
+            const { data, error } = await asingSupplierType(idpersona, 'proveedor');
+
+            if (error) {
+                return {
+                    status: false,
+                    message: 'Error al asignar el tipo de proveedor: ' + error,
+                };
+            }
+
+            return {
+                status: true,
+                message: 'Tipo de proveedor asignado exitosamente',
+            };
+        }
+    }),
+    addSupplierContact: defineAction({
+        input: z.object({
+            idpersona: z.number(),
+            telefonoProveedor: z.number(),
+            correoProveedor: z.string(),
+        }),
+        async handler({ idpersona, telefonoProveedor, correoProveedor }) {
+            const { data, error } = await addSupplierContact(idpersona, telefonoProveedor, correoProveedor);
+
+            if (error) {
+                return {
+                    status: false,
+                    message: 'Error al agregar el contacto del proveedor: ' + error,
+                };
+            }
+
+            return {
+                status: true,
+                message: 'Contacto del proveedor agregado exitosamente',
+            };
+        }
+    }),
+    addSupplierDirection: defineAction({
+        input: z.object({
+            idpersona: z.number(),
+            direccionProveedor: z.string(),
+            ciudadProveedor: z.string(),
+            codigoPostalProveedor: z.number(),
+        }),
+        async handler({ idpersona, direccionProveedor, ciudadProveedor, codigoPostalProveedor }) {
+            const { data, error } = await addSupplierDirection(idpersona, direccionProveedor, ciudadProveedor, codigoPostalProveedor);
+
+            if (error) {
+                return {
+                    status: false,
+                    message: 'Error al agregar la direccion del proveedor: ' + error,
+                };
+            }
+
+            return {
+                status: true,
+                message: 'Direccion del proveedor agregada exitosamente',
+            };
+        }
+    })
 });
--- src/models/almacen/entrada.ts
+++ src/models/almacen/entrada.ts
@@ -12,3 +12,73 @@ export async function getEntries() {
 
     return { data: entries, error: null };
 }
+
+export async function addSupplier(nombre: string, tipo_documento: string, nro_documento: number, estado: boolean) {
+    const { data, error } = await supabase
+        .from('personas')
+        .insert({
+            nombre: nombre,
+            tipo_documento: tipo_documento,
+            nro_documento: nro_documento,
+            estado: estado,
+        })
+        .select('idpersona')
+
+    if (error) {
+        console.error('Error al agregar el proveedor:', error);
+        return { data: null, error };
+    }
+
+    return { data: data[0].idpersona, error: null };
+}
+
+export async function addSupplierContact(idpersona: number, phone: number, email: string) {
+    const { data, error } = await supabase
+        .from('contacto')
+        .insert({
+            idpersona: idpersona,
+            phone: phone,
+            email: email,
+        });
+
+    if (error) {
+        console.error('Error al agregar el contacto del proveedor:', error);
+        return { data: null, error };
+    }
+
+    return { data: data, error: null };
+}
+
+export async function addSupplierDirection(idpersona: number, address: string, city: string, zip_code: number) {
+    const { data, error } = await supabase
+        .from('direccion')
+        .insert({
+            idpersona: idpersona,
+            address: address,
+            city: city,
+            zip_code: zip_code,
+        });
+
+    if (error) {
+        console.error('Error al agregar la dirección del proveedor:', error);
+        return { data: null, error };
+    }
+
+    return { data: data, error: null };
+}
+
+export async function asingSupplierType(idpersona: number, type: string) {
+    const { data, error } = await supabase
+        .from('tipo_persona')
+        .insert({
+            idpersona: idpersona,
+            type: type,
+        });
+
+    if (error) {
+        console.error('Error al asignar el tipo de proveedor:', error);
+        return { data: null, error };
+    }
+
+    return { data: data, error: null };
+}
